72cc81e43d80e8e1fe97644a6c695eb9387167d9
Minwoo <gapbu123@nate.com>
2023-03-06 04:42:11 +0900

    finished

--- 6. 문자열/CroatiaAlphabet(2941).py
+++ 6. 문자열/CroatiaAlphabet(2941).py
@@ -1,53 +1,16 @@
-message = input()
+s = input()
 
 count = 0
-j = 1
-
-for i in range(len(message)):
-    mark = 0
-    if len(message) == 1:
-        count+=1
-        break
-    if message[j] == 'j' and message[j-1] == 'l':
-        j += 1
-        count+=1
-        mark = 1
-    elif message [j] == '=' and message[j-1] == 'c':
-        j += 1
-        count+=1
-        mark = 1
-    elif message [j] == '-' and message[j-1] == 'c':
-        j += 1
-        count+=1
-        mark = 1
-    elif message [j] == '=' and message[j-1] == 'z' and message[j-2] == 'd':
-        j += 2
-        count+=1
-        mark = 1
-    elif message [j] == '-' and message[j-1] == 'd':
-        j += 1
-        count+=1
-        mark = 1
-    elif message [j] == 'j' and message[j-1] == 'n':
-        j += 1
-        count+=1
-        mark = 1
-    elif message [j] == '=' and message[j-1] == 's':
-        j += 1
-        count+=1
-        mark = 1
-    elif message [j] == '=' and message[j-1] == 'z':
-        j += 1
-        count+=1
-        mark = 1
+i = 0
+while i < len(s):
+    if s[i:i+2] in ['c=', 'c-', 'd-', 'lj', 'nj', 's=', 'z=']:
+        count += 1
+        i += 2
+    elif s[i:i+3] == 'dz=':
+        count += 1
+        i += 3
     else:
-        count+=1
-    j += 1
-    print(j, count)
-    if j == len(message) and mark == 0:
-        count+=1
-        break
-    elif j-1 == len(message) and mark == 1:
-        break
+        count += 1
+        i += 1
 
 print(count)
