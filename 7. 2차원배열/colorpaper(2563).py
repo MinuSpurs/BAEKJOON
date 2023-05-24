30618aee7f7fabc2afffa3adae98d4ce7f14258d
Minwoo Oh <gapbu123@nate.com>
2023-05-24 16:48:04 +0900

    commit

--- 7. 2차원배열/colorpaper(2563).py
+++ 7. 2차원배열/colorpaper(2563).py
@@ -3,20 +3,24 @@ import sys
 N = int(input())
 
 matrix = []
-result = N * 100
+result = 100
 
 for i in range(N):
     matrix.append(list(map(int, sys.stdin.readline().split())))
 
-for i in range(N):
-    for j in range(N):
+matrix.sort()
+i = 0
+
+while(i<=N):
+    for j in range(i, N):
         if i == j:
             continue
-        if result <= 100:
-            break
-        if (matrix[i][0] <= matrix[j][0] and matrix[i][0] + 10 >= matrix[j][0]):
-            print("위조건", i,j, (matrix[i][0] + 10) - matrix[j][0], 10 - (abs(matrix[i][1] - matrix[j][1])))
-            temp = ((matrix[i][0] + 10) - matrix[j][0]) * (10 - (abs(matrix[i][1] - matrix[j][1])))
-            result -= temp
-        
+        if matrix[i][0] <= matrix[j][0] and (matrix[i][0] + 10) >= matrix[j][0]:
+            result += (100 - (10 - (matrix[j][0] - matrix[i][0])) * (10 - abs((matrix[j][1] - matrix[i][1]))))
+            i += 1
+        else:
+            result += 100
+            i += 1
+    i += 1
+
 print(result)
