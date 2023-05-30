ba699ea838617c30f187cffeb62a4b6889b6599c
Minwoo Oh <gapbu123@nate.com>
2023-05-30 14:20:23 +0900

    finished

--- 8. 일반 수학 1/arithmetic_change(2745).py
+++ 8. 일반 수학 1/arithmetic_change(2745).py
@@ -1,3 +1,18 @@
-B, N = int(input().split())
+N, B = input().split()
 
-print(B,N)
+
+value = int(N, int(B))
+
+'''
+elif B > 2 and B <= 4:
+
+elif B > 4 and B <= 8:
+
+elif B > 8 and B <= 16:
+
+elif B > 16 and B <= 32:
+
+elif B > 32 and B <= 64:    
+'''
+
+print(value)
