482b4d794d66465913ee41efc270fc7ebf80fe52
Minwoo Oh <gapbu123@nate.com>
2023-06-01 17:48:07 +0900

    finished

--- 8. 일반 수학 1/find_fountain(1193).py
+++ 8. 일반 수학 1/find_fountain(1193).py
@@ -1,37 +1,30 @@
 x = int(input())
 
 up, down = 1, 1
-point = 1
-max_up, max_down = 1, 1
-pin = 1
+pin = 0
+max_value = 2
+up_point, down_point = 0, 1
+
 for i in range(x-1):
-    if point % 2 == 0 and pin == 1:
-        print('짝수 핀 1')
-        max_up += 1
-        up += 1
-        pin = 0
-        point += 1
-        continue
-    elif point % 2 == 1 and pin == 1:
-        print('홀수 핀 1')
-        max_down += 1
+    if down_point == 1:
+        down_point = 0
         down += 1
-        pin = 0
-        point += 1
-        continue
-
-    if down > up:
+        pin = 1
+    elif up_point == 1:
+        up_point = 0
+        up += 1
+        pin = 2
+    elif down_point == 0 and pin == 1:
+        up += 1
         down -= 1
-        up += 1
-        if (max_up == point) and max_up % 2 == 1:
-            pin = 1
-    elif up > down:
-        up -= 1
+        if up == max_value:
+            up_point = 1
+            max_value += 1
+    else:
         down += 1
-        if (max_down == point) and max_down % 2 == 0:
-            pin = 1
-
-    
-    print(point, max_up, max_down, up, down)
+        up -= 1
+        if down == max_value:
+            down_point = 1
+            max_value += 1
 
 print(str(up)+'/'+str(down))
