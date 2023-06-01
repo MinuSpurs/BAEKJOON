9d41a018a70ba4de589e87f9074690d726706b66
Minwoo <gapbu123@nate.com>
2023-06-01 22:30:03 +0900

    finished

--- 8. 일반 수학 1/find_fountain(1193).py
+++ 8. 일반 수학 1/find_fountain(1193).py
@@ -1,30 +1,28 @@
+import math
+
 x = int(input())
 
-up, down = 1, 1
-pin = 0
-max_value = 2
-up_point, down_point = 0, 1
+a = 1
+b = 1
+c = -x * 2
 
-for i in range(x-1):
-    if down_point == 1:
-        down_point = 0
-        down += 1
-        pin = 1
-    elif up_point == 1:
-        up_point = 0
-        up += 1
-        pin = 2
-    elif down_point == 0 and pin == 1:
-        up += 1
-        down -= 1
-        if up == max_value:
-            up_point = 1
-            max_value += 1
-    else:
-        down += 1
+r1 = (-b + (b**2-4*a*c)**0.5)/(2*a)
+s = ((2 + (math.ceil(r1) - 1)) * math.ceil(r1)) / 2
+up, down = 1, 1
+
+if math.ceil(r1) % 2 == 0:
+    up = math.ceil(r1)
+    i = int(s) - x
+    while i > 0:
         up -= 1
-        if down == max_value:
-            down_point = 1
-            max_value += 1
+        down += 1
+        i -= 1
+elif math.ceil(r1) % 2 == 1:
+    down = math.ceil(r1)
+    i = int(s) - x
+    while i > 0:
+        down -= 1
+        up += 1
+        i -= 1
 
 print(str(up)+'/'+str(down))
