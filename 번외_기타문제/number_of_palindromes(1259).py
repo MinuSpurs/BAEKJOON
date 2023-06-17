bec0855b539765badb792f2aa4a291b1f3cd9cc5
Minwoo <gapbu123@nate.com>
2023-06-18 04:14:12 +0900

    finished

---
+++ 번외_기타문제/number_of_palindromes(1259).py
@@ -0,0 +1,14 @@
+import sys
+
+while True:
+    mark = 0
+    word = sys.stdin.readline().rstrip()
+    if word == '0':
+        break
+    for i in range(len(word)//2 + 1):
+        if word[i] == word[len(word)-(i + 1)]:
+            if i == len(word)//2:
+                print('yes')
+        else:
+            print('no')
+            break
