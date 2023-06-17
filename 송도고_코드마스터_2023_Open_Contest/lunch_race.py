f7977d6b5bd9e796aaef49928b34fb9c13a023fa
Minwoo <gapbu123@nate.com>
2023-06-17 17:30:27 +0900

    finished

---
+++ 송도고_코드마스터_2023_Open_Contest/lunch_race.py
@@ -0,0 +1,15 @@
+n, m, k = map(int, input().split())
+
+distances = [0] * k  
+winning_class = 1 
+
+for i in range(k):
+    f, d = map(int, input().split())
+    distance = f + ((m + 1) - d)  
+    distances[i] = (distance, i+1)  
+
+distances.sort()
+
+winning_class = distances[0][1]
+
+print(winning_class)
