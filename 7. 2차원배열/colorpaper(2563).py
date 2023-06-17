31eea733e2e040f03128942ecf7372b5d3eb3214
Minwoo <gapbu123@nate.com>
2023-06-17 20:13:50 +0900

    finished

--- 7. 2차원배열/colorpaper(2563).py
+++ 7. 2차원배열/colorpaper(2563).py
@@ -1,21 +1,18 @@
-paper = [[0] * 100 for _ in range(100)]  # 100x100 크기의 흰색 도화지 배열 초기화
+paper = [[0] * 100 for _ in range(100)] 
 
-print(paper)
-"""
-n = int(input())  # 색종이의 수
+n = int(input())
 
 for _ in range(n):
     x, y = map(int, input().split())
     for i in range(x, x+10):
         for j in range(y, y+10):
-            paper[i][j] = 1  # 검은색 색종이가 있는 부분을 1로 설정
+            paper[i][j] = 1 
 
-area = 0  # 검은 영역의 넓이
+area = 0 
 
 for i in range(100):
     for j in range(100):
         if paper[i][j] == 1:
-            area += 1  # 1로 설정된 영역을 세어 넓이를 증가시킴
+            area += 1 
 
 print(area)
-"""
