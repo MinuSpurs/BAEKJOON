2249a25829672d0f02f7a158d23dac16e0782c9d
Minwoo <gapbu123@nate.com>
2023-06-28 20:44:45 +0900

    finished

---
+++ 번외_기타문제/Football_Team(5358).py
@@ -0,0 +1,17 @@
+while True:
+    name = input()
+    if not name:
+        break
+    result = ""
+    for ch in name:
+        if ch == 'i':
+            result += 'e'
+        elif ch == 'e':
+            result += 'i'
+        elif ch == 'I':
+            result += 'E'
+        elif ch == 'E':
+            result += 'I'
+        else:
+            result += ch
+    print(result)
