e984a141f6ec294c21d953cf270607a937eeffe3
Minwoo <gapbu123@nate.com>
2023-08-06 17:26:06 +0900

    finished

--- 19. 스택, 큐, 덱/Josephus_problem_0(11866).py
+++ 19. 스택, 큐, 덱/Josephus_problem_0(11866).py
@@ -5,10 +5,10 @@ n, k = map(int, input().split())
 queue = deque([i + 1 for i in range(n)])
 jose = []
 
-while queue:  # queue에 원소가 남아있는 동안 반복
-    for _ in range(k - 1):  # k-1번째 요소를 처리하기 위한 위치 변경
+while queue:
+    for _ in range(k - 1): 
         queue.append(queue.popleft())
     
-    jose.append(queue.popleft())  # 요세푸스 순열에 선택된 원소 추가
+    jose.append(queue.popleft())
 
-print("<" + ", ".join(map(str, jose)) + ">")  # 출력 양식에 맞게 출력
+print("<" + ", ".join(map(str, jose)) + ">") 
