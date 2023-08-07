6c0ba4caf2cc93bd9de2e1da45167bb9300887dd
Minwoo <gapbu123@nate.com>
2023-08-07 12:41:07 +0900

    finished

--- 19. 스택, 큐, 덱/deque2(28279).py
+++ 19. 스택, 큐, 덱/deque2(28279).py
@@ -6,16 +6,16 @@ n = int(input())
 queue = deque([])
 
 for i in range(n):
-    op = input()
+    op = sys.stdin.readline().rstrip()
 
     if op[:1] == '1':
         queue.insert(0, int(op[2:]))
     elif op[:1] == '2':
         queue.insert(len(queue), op[2:])
     elif op == '3':
-        print(-1 if len(queue) == -1 else queue.popleft())
+        print(-1 if len(queue) == 0 else queue.popleft())
     elif op == '4':
-        print(-1 if len(queue) == -1 else queue.pop())
+        print(-1 if len(queue) == 0 else queue.pop())
     elif op == '5':
         print(len(queue))
     elif op == '6':
