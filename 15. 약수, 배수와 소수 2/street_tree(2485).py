1e45eb757cd7e440c26df24bbc1569d99b1b8b32
Minwoo <gapbu123@nate.com>
2023-08-10 18:38:36 +0900

    finished

--- 15. 약수, 배수와 소수 2/street_tree(2485).py
+++ 15. 약수, 배수와 소수 2/street_tree(2485).py
@@ -1,31 +1,24 @@
-import sys
+import math
 
 N = int(input())
 
 tree_list = [0] * N
 for i in range(N):
-    tree = int(sys.stdin.readline())
+    tree = int(input())
     tree_list[i] = tree
 
 sorted_tree = sorted(tree_list)
 
-diff = [0] * (N-1)
-for i in range(N-1):
-    diff[i] = sorted_tree[i+1] - sorted_tree[i]
+diff = [0] * (N - 1)
+for i in range(N - 1):
+    diff[i] = sorted_tree[i + 1] - sorted_tree[i]
 
-sorted_diff = sorted(diff)
-gcd = sorted_tree[1] - sorted_tree[0]
-for i in range(len(sorted_diff) - 1):
-    a, b = sorted_diff[i+1], sorted_diff[i]
-    while b > 0:
-        a, b = b, a%b
-    if gcd > a:
-        gcd = a
+gcd = diff[0]
+for i in range(1, len(diff)):
+    gcd = math.gcd(gcd, diff[i])
 
-i = 0
 cnt = 0
-
-for i in sorted_diff:
-    cnt += (i//gcd) - 1
+for i in diff:
+    cnt += (i // gcd) - 1
 
 print(cnt)
