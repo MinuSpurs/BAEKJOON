05c1b54459b04c57876d882b2a23251d97c7ba7a
Minwoo <gapbu123@nate.com>
2023-08-30 13:47:48 +0900

    finished

--- 7. 2차원배열/matrix_plus(2738).py
+++ 7. 2차원배열/matrix_plus(2738).py
@@ -10,9 +10,11 @@ for j in range(N):
 for j in range(N):
     matrix_2.append(list(map(int, sys.stdin.readline().split())))
 
-for i in range(N):
+'''for i in range(N):
     result.append([])
     for j in range(M):
         result[i].append(matrix_1[i][j] + matrix_2[i][j])
         print(result[i][j], end=' ')
-    print()
+    print()'''
+
+print(matrix_1 + matrix_2)
