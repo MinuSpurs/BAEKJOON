74dca061a3220b2040083a97a90659f562c4c0c7
오민우 <minwoo@omin-uui-MacBookAir.local>
2024-03-08 18:56:58 +0900

    finished

---
+++ DxLab_스터디/BFS/connected_component(11724).py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def bfs(graph, start, visited, cnt):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+    cnt += 1
+    return cnt
+
+n, m = map(int, input().split())
+
+graph = [[] for _ in range(n)]
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a-1].append(b-1)
+    graph[b-1].append(a-1)
+
+visited = [False] * n
+
+cnt = 0
+idx = 0
+
+while False in visited:
+    if visited[idx] == False:
+        cnt = bfs(graph, idx, visited, cnt)
+    idx += 1
+
+print(cnt)
