cf167f71c14fc7d39c514de2dca85efad5099945
오민우 <minwoo@omin-uui-MacBookAir.local>
2024-03-08 19:32:57 +0900

    finished

--- DxLab_스터디/BFS/connected_component(11724).py
+++ DxLab_스터디/BFS/connected_component(11724).py
@@ -1,3 +1,4 @@
+import sys
 from collections import deque
 
 def bfs(graph, start, visited, cnt):
@@ -13,11 +14,11 @@ def bfs(graph, start, visited, cnt):
     cnt += 1
     return cnt
 
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().split())
 
 graph = [[] for _ in range(n)]
 for i in range(m):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     graph[a-1].append(b-1)
     graph[b-1].append(a-1)
 
