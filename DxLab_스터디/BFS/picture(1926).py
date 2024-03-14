6aaac4c139b2f5d155a67adef5f9baf2ef9d079a
오민우 <minwoo@omin-uui-MacBookAir.local>
2024-03-14 14:19:22 +0900

    finished

---
+++ DxLab_스터디/BFS/picture(1926).py
@@ -0,0 +1,97 @@
+'''import sys
+from collections import deque
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = []
+picture_list = []
+
+for i in range(n):
+    graph.append(list(map(int, sys.stdin.readline().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    shape = 0
+    while queue:
+        x ,y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            if graph[nx][ny] == 1:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+                shape += 1
+    return shape
+
+picture_count = 0
+max_picture_area = 0
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            picture_count += 1
+            area = bfs(i, j)
+            print(area)
+            max_picture_area = max(max_picture_area, area)
+
+print(picture_count)
+print(max_picture_area)'''
+
+from collections import deque
+
+n, m = map(int, input().split())
+
+graph = []
+
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    shape = 1
+    graph[x][y] = 0 
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                continue
+
+            if graph[nx][ny] == 0:
+                continue
+
+            shape += 1
+            queue.append((nx, ny))
+            graph[nx][ny] = 0 
+
+    return shape
+
+picture_count = 0
+max_picture_area = 0
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            picture_count += 1
+            area = bfs(i, j)
+            max_picture_area = max(max_picture_area, area)
+
+print(picture_count)
+print(max_picture_area)
